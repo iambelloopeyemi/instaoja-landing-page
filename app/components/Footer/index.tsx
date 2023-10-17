fad2ee21891c9897c83dbdd9c493dd782bfb817b
Opeyemi Bello <opeyemiemmanuelbello@gmail.com>
2023-10-17 19:05:11 +0100

    create footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,3 +1,18 @@
+import Logo from "../Logo";
+
 export default function Footer() {
-  return <footer></footer>;
+  const currentYear: number = new Date().getFullYear();
+
+  return (
+    <footer className="bg-nyanza px-20 py-10">
+      <div className="flex justify-center items-center mb-10">
+        <Logo />
+      </div>
+      <div className="border-t py-2.5">
+        <p className="text-center font-light text-sm">
+          © {currentYear} InstaOja Limited. All right reserved
+        </p>
+      </div>
+    </footer>
+  );
 }
